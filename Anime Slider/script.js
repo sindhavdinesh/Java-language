6360c5b987529fd685aefc799cd6dc72f89abf27
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-02-09 09:32:17 +0530

    Add files via upload

--- Anime Slider/script.js
+++ Anime Slider/script.js
@@ -4,6 +4,19 @@ const dotsContainer = document.getElementById("dots");
 
 let index = 0;
 
+const Images = [
+  { path: "https://images.hdqwalls.com/wallpapers/bthumb/itachi-uchiha-silent-protector-lm.jpg" },
+  { path: "https://images.hdqwalls.com/wallpapers/bthumb/sasuke-uchiha-mh.jpg" },
+  { path: "https://images.hdqwalls.com/download/sharingan-naruto-tf-2560x1440.jpg" },
+  { path: "https://images.hdqwalls.com/wallpapers/obito-uchiha-mask-of-the-fallen-p6.jpg" },
+  { path: "https://images.hdqwalls.com/download/satoru-gojo-the-sorcerer-supreme-56-1366x768.jpg" }
+];
+
+function displayImage(i) {
+  image.src = Images[i].path;
+  bg.style.backgroundImage = `url(${Images[i].path})`;
+}
+
 /* Create dots */
 for (let i = 0; i < totalSlides; i++) {
   const dot = document.createElement("span");
@@ -40,5 +53,7 @@ function goToSlide(i) {
   updateSlider();
 }
 
+
 /* Start */
 updateSlider();
+
